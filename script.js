cd998fd71b486542f924b255fdb8608a9df1890a
valruizb05 <ing.valeriaruiz5@gmail.com>
2024-08-04 19:23:41 -0600

    Navbar

---
+++ script.js
@@ -0,0 +1,50 @@
+//alert("Hola Mundo <3");
+
+// se ocupa let porque var hace que las variables vivan en otros archivos
+
+let app = document.getElementById('app');
+
+let typewriter = new Typewriter(app, {
+    loop: true,
+    delay: 75,
+});
+
+// aqui permite que la etiqueta sea manipulada
+//reconoces una accion tiene paréntesis después del nombre de la accion
+// a veces tiene el operador punto
+//typewriter
+
+typewriter
+    .pauseFor(2500) //milisegundos 2.5 segundos
+    .typeString('Karla Yaneth Cruz Sandoval')
+    .pauseFor(300)
+    .deleteAll()
+    .typeString('Desarrolladora FRONTEND JR ')
+    .pauseFor(1000)
+    .start();
+
+
+let frase = document.getElementById('frase');
+
+let typewriterFrase = new Typewriter(frase, {
+    loop: true,
+    delay: 75,
+});
+
+// aqui permite que la etiqueta sea manipulada
+//reconoces una accion tiene paréntesis después del nombre de la accion
+// a veces tiene el operador punto
+//typewriter
+
+typewriterFrase
+    .pauseFor(2500) //milisegundos 2.5 segundos
+    .typeString('"Hay algunas flores que solo ves cuando tomas desvíos."')
+    .pauseFor(400)
+    .deleteAll()
+    .typeString('Tanaka Seako ')
+    .pauseFor(1000)
+    .start();
+
+
+
+
